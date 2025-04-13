d6248d4243ba9f4fbfa8c8503afe8bb1b0a5876c
SylvioMello <sylvio@mello.net.br>
2025-04-13 18:23:11 -0300

    commenting aws resources

--- main.tf
+++ main.tf
@@ -25,63 +25,65 @@ locals {
   subnet_id          = "subnet-06db9c85ba061b6f2" # this can be found at the VPC screen
 }
 
-resource "aws_security_group" "instance-sg" {
-  name        = "Ks Node SG"
-  description = "SG for Kubeadm Nodes"
+# Commenting the resources to avoid extra charging after concluding the Gratuation Project =D
 
-  dynamic "ingress" {
-    for_each = toset(range(length(local.inbound_from_port)))
-    content {
-      from_port   = local.inbound_from_port[ingress.key]
-      to_port     = local.inbound_to_port[ingress.key]
-      protocol    = local.inbound_protocol[ingress.key]
-      cidr_blocks = [local.inbound_cidr[ingress.key]]
-    }
-  }
+# resource "aws_security_group" "instance-sg" {
+#   name        = "Ks Node SG"
+#   description = "SG for Kubeadm Nodes"
 
-  dynamic "egress" {
-    for_each = toset(range(length(local.outbound_from_port)))
-    content {
-      from_port   = local.outbound_from_port[egress.key]
-      to_port     = local.outbound_to_port[egress.key]
-      protocol    = local.outbound_protocol[egress.key]
-      cidr_blocks = [local.outbound_cidr[egress.key]]
-    }
-  }
-}
+#   dynamic "ingress" {
+#     for_each = toset(range(length(local.inbound_from_port)))
+#     content {
+#       from_port   = local.inbound_from_port[ingress.key]
+#       to_port     = local.inbound_to_port[ingress.key]
+#       protocol    = local.inbound_protocol[ingress.key]
+#       cidr_blocks = [local.inbound_cidr[ingress.key]]
+#     }
+#   }
 
-resource "aws_instance" "srsRAN_K8s" {
-  ami                    = "ami-0e1bed4f06a3b463d" #ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*
-  instance_type          = "t3.xlarge"
-  subnet_id              = local.subnet_id
-  key_name               = local.key_name
-  vpc_security_group_ids = [aws_security_group.instance-sg.id]
+#   dynamic "egress" {
+#     for_each = toset(range(length(local.outbound_from_port)))
+#     content {
+#       from_port   = local.outbound_from_port[egress.key]
+#       to_port     = local.outbound_to_port[egress.key]
+#       protocol    = local.outbound_protocol[egress.key]
+#       cidr_blocks = [local.outbound_cidr[egress.key]]
+#     }
+#   }
+# }
 
-  # user_data for K8s installation
-  user_data = <<-EOF
-    #cloud-config
-    hostname: "srsRAN_K8s"
-  EOF
+# resource "aws_instance" "srsRAN_K8s" {
+#   ami                    = "ami-0e1bed4f06a3b463d" #ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*
+#   instance_type          = "t3.xlarge"
+#   subnet_id              = local.subnet_id
+#   key_name               = local.key_name
+#   vpc_security_group_ids = [aws_security_group.instance-sg.id]
 
-  tags = {
-    Name = "srsRAN_K8s"
-  }
-}
+#   # user_data for K8s installation
+#   user_data = <<-EOF
+#     #cloud-config
+#     hostname: "srsRAN_K8s"
+#   EOF
 
-resource "aws_instance" "srsRAN_BareMetal" {
-  ami                    = "ami-0e1bed4f06a3b463d" #ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*
-  instance_type          = "t3.large"
-  subnet_id              = local.subnet_id
-  key_name               = local.key_name
-  vpc_security_group_ids = [aws_security_group.instance-sg.id]
+#   tags = {
+#     Name = "srsRAN_K8s"
+#   }
+# }
 
-  # user_data for K8s installation
-  user_data = <<-EOF
-    #cloud-config
-    hostname: "srsRAN_BareMetal"
-  EOF
+# resource "aws_instance" "srsRAN_BareMetal" {
+#   ami                    = "ami-0e1bed4f06a3b463d" #ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*
+#   instance_type          = "t3.large"
+#   subnet_id              = local.subnet_id
+#   key_name               = local.key_name
+#   vpc_security_group_ids = [aws_security_group.instance-sg.id]
 
-  tags = {
-    Name = "srsRAN_BareMetal"
-  }
-}
+#   # user_data for K8s installation
+#   user_data = <<-EOF
+#     #cloud-config
+#     hostname: "srsRAN_BareMetal"
+#   EOF
+
+#   tags = {
+#     Name = "srsRAN_BareMetal"
+#   }
+# }
